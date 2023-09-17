323e740b82971873a30db0d9af1b757f712c75b9
REBECCA YU <rebeccayu2015@gmail.com>
2023-09-16 20:02:59 -0400

    User authentication

---
+++ Clean Closet/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  Clean Closet
+//
+//  Created by Rebecca Yu on 9/16/23.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginClicked(_ sender: UIButton) {
+        guard let email = userTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                print("error")
+            }
+            else {
+                // Go to our home screen
+                self.performSegue(withIdentifier: "goToNext", sender: self)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Clean Closet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean Closet
+//
+//  Created by Ayat Noor on 9/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
